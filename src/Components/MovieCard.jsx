c47f76516cc4bcdb971333b67ffa7a95e549850a
ChaabaneAmmar21 <chaabaneammar123@gmail.com>
2022-08-20 19:19:33 +0100

    first commit

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -2,16 +2,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
-
+import { Link} from "react-router-dom";
 function MovieCard(props) {
  
   return (
 
 
   <div className="card" style={{width:"30%" }}>
-    <div className="image">
+   <Link to={`/${props.el.id}`}><div className="image">
       <img src={props.el.posterURL} height="300px" alt=''/>
-    </div>
+    </div></Link> 
     <div className="card-inner">
       <div className="header">
         <h2>{props.el.title}</h2>
